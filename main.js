37d42929c9ab9b024d7b9233d8892dbcc08a9809
Wouter Verweirder <wouter.verweirder@gmail.com>
2020-12-15 10:06:04 +0100

    added 8 screen electron version

---
+++ main.js
@@ -0,0 +1,54 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, screen} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  let displays = screen.getAllDisplays()
+
+  // only keep 4K windows
+  displays = displays.filter(o => o.bounds.height === 2160).sort();
+  
+  const spannedDisplay = displays.reduce((prev, curr) => ({ bounds: { x: Math.min(prev.bounds.x, curr.bounds.x), y: Math.min(prev.bounds.y, curr.bounds.y)}, size: { width: prev.size.width + curr.size.width, height: Math.max(prev.size.height, curr.size.height)}}), { size: { width: 0, height: 0 }, bounds: { x: 0, y: 0 }});  
+
+  // Create the window, make it span all displays
+  const mainWindow = new BrowserWindow({
+    x: spannedDisplay.bounds.x,
+    y: spannedDisplay.bounds.y,
+    width: spannedDisplay.size.width,
+    height: spannedDisplay.size.height,
+    frame: false,
+    titleBarStyle: 'customButtonsOnHover',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('demo20-threejs-8-portrait.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
