52ecaf005b996fbc148f2f5b228ee8b6af295f06
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-05-06 08:37:24 +0200

    show either one video or one image in project assets texture

--- public/js/classes/textures/ProjectAssetsTexture.js
+++ public/js/classes/textures/ProjectAssetsTexture.js
@@ -5,21 +5,21 @@ class ProjectAssetsTexture extends ScreenTexture {
   async init() {
 
     if (this.userData.data && this.userData.data.length > 0) {
-      // // take the first image asset
-      // const imageAssets = this.userData.data.filter(asset => asset.mime.includes('image'));
-      // if (imageAssets.length > 0) {
-      //   // draw the image in the center of the canvas
-      //   const image = await loadImage(imageAssets[0].url);
-      //   const offsetX = (this.canvas.width - image.width) / 2;
-      //   const offsetY = (this.canvas.height - image.height) / 2;
-      //   this.ctx.drawImage(image, offsetX, offsetY);
-      // }
-      const videoAssets = this.userData.data.filter(asset => asset.mime.includes('video'));
-      if (videoAssets.length > 0) {
-        const videoAsset = videoAssets[0];
+      // take the first asset
+      const asset = this.userData.data[0];
+      const isImage = asset.mime.includes('image');
+      const isVideo = asset.mime.includes('video');
+      if (isImage) {
+        // draw the image in the center of the canvas
+        const image = await loadImage(asset.url);
+        const offsetX = (this.canvas.width - image.width) / 2;
+        const offsetY = (this.canvas.height - image.height) / 2;
+        this.ctx.drawImage(image, offsetX, offsetY);
+      } else if (isVideo) {
         const video = document.createElement('video');
         video.autoplay = true;
-        video.src = videoAsset.url;
+        video.loop = true;
+        video.src = asset.url;
 
         const updateVideo = () => {
 
