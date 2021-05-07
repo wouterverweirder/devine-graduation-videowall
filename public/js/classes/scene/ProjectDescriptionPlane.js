71f15b00df626535a7d8de6bc8c7a77926425ff3
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-05-07 13:35:45 +0200

    updated line height description

--- public/js/classes/scene/ProjectDescriptionPlane.js
+++ public/js/classes/scene/ProjectDescriptionPlane.js
@@ -14,16 +14,19 @@ class ProjectDescriptionPlane extends VisualBase {
     this.ctx = ctx;
     this.texture = texture;
 
-    const marginLeft = 100;
-    const marginRight = 100;
-    const marginTop = 100;
+    const marginLeft = 50;
+    const marginRight = 50;
+    const marginTop = 50;
 
-    let yPos = 55 + marginTop;
+    const fontSize = 55;
+    const lineHeight = 70;
+
+    let yPos = fontSize + marginTop;
 
     const canvasObjects = [
       {
         type: 'text',
-        font: '55px "Embedded Space Grotesk"',
+        font: `${fontSize}px "Embedded Space Grotesk"`,
         fillStyle: 'white',
         content: 'Project Info',
         x: marginLeft,
@@ -34,7 +37,7 @@ class ProjectDescriptionPlane extends VisualBase {
     yPos += 200;
     const paragraphs = this.props.data.description.split("\n");
     paragraphs.forEach(paragraph => {
-      this.ctx.font = '55px "Embedded Space Grotesk"';
+      this.ctx.font = `${fontSize}px "Embedded Space Grotesk"`;
       const lines = getLines(this.ctx, paragraph.trim(), this.canvas.width - marginLeft - marginRight);
       lines.forEach(line => {
         canvasObjects.push({
@@ -45,9 +48,9 @@ class ProjectDescriptionPlane extends VisualBase {
           x: marginLeft,
           y: yPos
         })
-        yPos += 55;
+        yPos += lineHeight;
       });
-      yPos += 55 / 2;
+      yPos += lineHeight / 2;
     });
 
     // draw my basic displaylist to the screen
