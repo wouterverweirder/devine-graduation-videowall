7ceed6a79a23993d1f3087ec63b36d3eac273b7a
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-05-09 19:49:48 +0200

    calculateBoundsOfAllScreenCameras

--- public/js/functions/createCamerasForConfig.js
+++ public/js/functions/createCamerasForConfig.js
@@ -56,8 +56,33 @@ const createCamerasForConfig = async (config) => {
   return cameras;
 };
 
+const calculateBoundsOfAllScreenCameras = (screenCameras) => {
+  let left = 0, right = 0, bottom = 0, top = 0;
+  screenCameras.forEach(screenCamera => {
+    if (screenCamera.props.rotation.z !== 0) {
+      left = Math.min(screenCamera.object3D.position.x + screenCamera.object3D.bottom, left);
+      right = Math.max(screenCamera.object3D.position.x + screenCamera.object3D.top, right);
+      bottom = Math.min(screenCamera.object3D.position.y + screenCamera.object3D.left, bottom);
+      top = Math.max(screenCamera.object3D.position.y + screenCamera.object3D.right, top);
+    } else {
+      left = Math.min(screenCamera.object3D.position.x + screenCamera.object3D.left, left);
+      right = Math.max(screenCamera.object3D.position.x + screenCamera.object3D.right, right);
+      bottom = Math.min(screenCamera.object3D.position.y + screenCamera.object3D.bottom, bottom);
+      top = Math.max(screenCamera.object3D.position.y + screenCamera.object3D.top, top);
+    }
+  });
+  const totalBounds = {
+    width: right - left,
+    height: top - bottom
+  };
+  totalBounds.x = left + (totalBounds.width / 2);
+  totalBounds.y = bottom + (totalBounds.height / 2);
+  return totalBounds;
+}
+
 export {
   getBoundsForSize,
   getSizeForBounds,
-  createCamerasForConfig
+  createCamerasForConfig,
+  calculateBoundsOfAllScreenCameras
 }
