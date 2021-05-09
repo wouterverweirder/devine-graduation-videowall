cb269a22ccbbb868f0b7ba0109f3cc5727b00f59
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-05-09 19:28:24 +0200

    move scene to projectorapplication

--- public/js/classes/ProjectorApplication.js
+++ public/js/classes/ProjectorApplication.js
@@ -7,6 +7,8 @@ const BLACK = new THREE.Color(0, 0, 0);
 class ProjectorApplication extends Application {
 
   setupApplicationSpecificUI = () => {
+    this.scene = new THREE.Scene();
+    
     const outputCanvas = document.getElementById('output-canvas');
     outputCanvas.width = this.config.appDimensions.width;
     outputCanvas.height = this.config.appDimensions.height;
