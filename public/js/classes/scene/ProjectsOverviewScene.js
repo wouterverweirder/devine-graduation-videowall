21c8d4b1c97973ffe2eaac749ae62d1d178862af
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-06-09 20:50:40 +0200

    use threejs layers for projects overview

--- public/js/classes/scene/ProjectsOverviewScene.js
+++ public/js/classes/scene/ProjectsOverviewScene.js
@@ -1,57 +1,35 @@
 import { gsap, Power1 } from '../../gsap/src/index.js';
 
-import { getScreenCamerasForRoles } from "../../functions/screenUtils.js";
-import { createPlaneForScreen } from "../../functions/createPlaneForScreen.js";
+import { getScreenCamerasForRoles, calculateScaleForScreenConfig } from "../../functions/screenUtils.js";
 import { ScreenRole } from "../../consts/ScreenRole.js";
 import { SceneBase, SceneState } from "./SceneBase.js";
-import { loadImage } from '../../functions/loadImage.js';
-import { PlaneType } from '../../consts/PlaneType.js';
-
-const loadProfilePictureWithVariants = async (url) => {
-  const picture = {};
-  picture.orig = await loadImage(url);
-  // resized and scaled versions
-  const scale = 9 / 16;
-  picture.forLandscapeStack = new OffscreenCanvas(1920/2, 1080);
-  {
-    const ctx = picture.forLandscapeStack.getContext('2d');
-    ctx.scale(scale, scale);
-    ctx.translate((ctx.canvas.width - picture.orig.width * scale) / 2, (ctx.canvas.height - picture.orig.height * scale) / 2);
-    ctx.drawImage(picture.orig, 0, 0);
-  }
-  picture.forPortraitStack = new OffscreenCanvas(1080, 1920/2);
-  {
-    const ctx = picture.forPortraitStack.getContext('2d');
-    ctx.scale(scale, scale);
-    ctx.translate((ctx.canvas.width - picture.orig.width * scale) / 2, (ctx.canvas.height - picture.orig.height * scale) / 2);
-    ctx.drawImage(picture.orig, 0, 0);
-  }
-  return picture;
-};
+import { ImagePlane } from './objects/ImagePlane.js';
 
 class ProjectsOverviewScene extends SceneBase {
 
-  screenPlanes = [];
-  allProjectImages = [];
-  nonVisibleImages = [];
-  visibleImages = [];
+  allProjectPlanes = [];
+  nonVisiblePlanes = [];
+  visiblePlanes = [];
   animationTimeoutId = false;
 
   async _executeStateName(stateName) {
     if (stateName === SceneState.LOAD) {
 
-      // load all images
+      // create a plane for each project
       for (let index = 0; index < this.projects.length; index++) {
         const project = this.projects[index];
-
-        const picture = await loadProfilePictureWithVariants(project.profilePicture.url);
-        const image = {
-          picture,
-          x: 0,
-          y: 0
+        const props = {
+          name: `project-overview-${project.id}-${index}`,
+          textureSize: {
+            x: 1920,
+            y: 1920
+          },
+          url: project.profilePicture.url
         };
-        this.allProjectImages.push(image);
-        this.nonVisibleImages.push(image);
+        const plane = new ImagePlane(props.name, props);
+        await plane.init();
+        this.allProjectPlanes.push(plane);
+        this.nonVisiblePlanes.push(plane);
       }
     } else if (stateName === SceneState.INTRO) {
       const cameras = getScreenCamerasForRoles(this.cameras, [
@@ -62,50 +40,22 @@ class ProjectsOverviewScene extends SceneBase {
         ScreenRole.LANDSCAPE_SCREENSHOTS,
         ScreenRole.VIDEOS,
       ]);
-      // create a screen plane per camera
+      // add two planes per camera
       for (const camera of cameras) {
-        const screenConfig = this.screenConfigsById[camera.id];
-        const screenPlane = await createPlaneForScreen({
-          data: {
-            id: `project-overview-screen-${camera.id}`,
-            type: PlaneType.CANVAS,
-            color: 0x000000,
-          },
-          screenConfig
-        });
-        await screenPlane.init();
-        this.screenPlanes.push(screenPlane);
-
         for (let i = 0; i < 2; i++) {
-          if (this.nonVisibleImages.length === 0) {
+          if (this.nonVisiblePlanes.length === 0) {
             break;
           }
           const isFirstItemOnScreen = (i === 0);
-          const image = this.nonVisibleImages.shift();
-          image.plane = screenPlane;
-          image.camera = camera;
-          image.isFirstItemOnScreen = isFirstItemOnScreen;
-
-          const isLandscape = !(camera.props.rotation.z !== 0);
-
-          const props = this.generatePropsForScreen(camera, isFirstItemOnScreen);
-
-          image.x = props.position.x;
-          image.y = props.position.y;
-
-          if (isLandscape) {
-            image.plane.ctx.drawImage(image.picture.forLandscapeStack, image.x, image.y);
-          } else {
-            image.plane.ctx.drawImage(image.picture.forPortraitStack, image.x, image.y);
-          }
-
-          this.visibleImages.push(image);
+          const plane = this.nonVisiblePlanes.shift();
+          plane.applyProps(this.generatePropsForScreen(camera, isFirstItemOnScreen));
+          plane.customData.camera = camera;
+          plane.customData.isFirstItemOnScreen = isFirstItemOnScreen;
+          this.visiblePlanes.push(plane);
         }
-
-        screenPlane.texture.needsUpdate = true;
       }
 
-      this.screenPlanes.forEach(plane => {
+      this.visiblePlanes.forEach(plane => {
         this.addObject(plane);
       });
 
@@ -114,32 +64,51 @@ class ProjectsOverviewScene extends SceneBase {
 
     } else if (stateName === SceneState.OUTRO) {
       this.killAnimationTimeout();
-      this.screenPlanes.forEach(plane => {
+      this.visiblePlanes.forEach(plane => {
         this.removeObject(plane);
       });
     }
   }
 
   generatePropsForScreen(camera, isFirstItemOnScreen) {
+    const screenConfig = this.screenConfigsById[camera.id];
+    const screenScale = calculateScaleForScreenConfig(screenConfig);
     const isLandscape = !(camera.props.rotation.z !== 0);
+    const layers = (camera.props.layers) ? camera.props.layers.concat() : false;
     const position = {
-      x: 0,
-      y: 0
+      x: screenConfig.camera.position[0],
+      y: screenConfig.camera.position[1],
+      z: 0
     };
-    const size = {
-      x: 1920 / 2,
-      y: 1920 / 2
-    }
+    const scale = {
+      x: screenScale.x,
+      y: screenScale.y
+    };
+    const anchor = {
+      x: 0.5,
+      y: 0.5
+    };
+    const fixedRepeat = {
+      x: 16/9/2,
+      y: 16/9/2
+    };
     if (isLandscape) {
-      size.y = 1080;
-      position.x += isFirstItemOnScreen ? 0 : 1920 / 2;
+      scale.x *= .5;
+      position.x += isFirstItemOnScreen ? -scale.x/2 : scale.x/2;
+      fixedRepeat.y = false;
+      anchor.y = (Math.random() < .5) ? 1 : 0;
     } else {
-      size.x = 1080;
-      position.y += isFirstItemOnScreen ? 0 : 1920 / 2;
+      scale.y *= .5;
+      position.y += isFirstItemOnScreen ? -scale.y/2 : scale.y/2;
+      fixedRepeat.x = false;
+      anchor.x = (Math.random() < .5) ? 1 : 0;
     }
     return {
+      layers,
       position,
-      size
+      scale,
+      fixedRepeat,
+      anchor
     };
   };
 
@@ -156,72 +125,73 @@ class ProjectsOverviewScene extends SceneBase {
   };
 
   animationTimeoutCb() {
-    if (this.nonVisibleImages.length === 0) {
+    if (this.nonVisiblePlanes.length === 0) {
       return;
     }
     // choose a random item to replace
-    const index = Math.floor(Math.random() * this.visibleImages.length);
-    if (index >= this.visibleImages.length) {
+    const index = Math.floor(Math.random() * this.visiblePlanes.length);
+    if (index >= this.visiblePlanes.length) {
       return;
     }
-    const oldImage = this.visibleImages[index];
-    const newImage = this.nonVisibleImages.shift();
-
-    newImage.camera = oldImage.camera;
-    newImage.isFirstItemOnScreen = oldImage.isFirstItemOnScreen;
-    newImage.plane = oldImage.plane;
-
-    const camera = oldImage.camera;
-    const isFirstItemOnScreen = oldImage.isFirstItemOnScreen;
+    const oldPlane = this.visiblePlanes[index];
+    const newPlane = this.nonVisiblePlanes.shift();
+    newPlane.customData.camera = oldPlane.customData.camera;
+    newPlane.customData.isFirstItemOnScreen = oldPlane.customData.isFirstItemOnScreen;
+    const camera = oldPlane.customData.camera;
     const isLandscape = !(camera.props.rotation.z !== 0);
     // animate
+    console.log(oldPlane.props);
     const tl = gsap.timeline({
       onUpdate: () => {
-
-        if (isLandscape) {
-          oldImage.plane.ctx.drawImage(oldImage.picture.forLandscapeStack, oldImage.x, oldImage.y);
-          newImage.plane.ctx.drawImage(newImage.picture.forLandscapeStack, newImage.x, newImage.y);
-        } else {
-          oldImage.plane.ctx.drawImage(oldImage.picture.forPortraitStack, oldImage.x, oldImage.y);
-          newImage.plane.ctx.drawImage(newImage.picture.forPortraitStack, newImage.x, newImage.y);
-        }
-
-        newImage.plane.texture.needsUpdate = true;
+        oldPlane.applyProps(oldPlane.props);
+        newPlane.applyProps(newPlane.props);
       },
       onComplete: () => {
-        const indexToRemove = this.visibleImages.indexOf(oldImage);
-        if (indexToRemove >= this.visibleImages.length) {
+        const indexToRemove = this.visiblePlanes.indexOf(oldPlane);
+        if (indexToRemove >= this.visiblePlanes.length) {
           return;
         }
-        this.visibleImages.splice(indexToRemove, 1);
-        this.nonVisibleImages.push(oldImage);
+        this.visiblePlanes.splice(indexToRemove, 1);
+        this.removeObject(oldPlane);
+        this.nonVisiblePlanes.push(oldPlane);
       }
     });
-
-    newImage.x = oldImage.x;
-    newImage.y = oldImage.y;
-
-    const slideDuration = 2;
-    const targetPropsOldImage = {};
-    const targetPropsNewImage = {
-      x: oldImage.x,
-      y: oldImage.y
+    const targetPropsOldPlane = {
+      position: {
+        x: oldPlane.props.position.x,
+        y: oldPlane.props.position.y,
+      }
     };
-    const direction = (Math.random() < .5) ? 1 : -1;
+    const setPropsOldPlane = {};
+    const setPropsNewPlane = this.generatePropsForScreen(camera, newPlane.customData.isFirstItemOnScreen);
+    const targetPropsNewPlane = JSON.parse(JSON.stringify(setPropsNewPlane));
+
+    const slideDuration = 1;
     if (isLandscape) {
-      // move vertically
-      targetPropsOldImage.y = direction * oldImage.picture.forLandscapeStack.height;
-      newImage.y -= direction * newImage.picture.forLandscapeStack.height;
+      if (setPropsNewPlane.anchor.y < .5) {
+        targetPropsOldPlane.position.y += oldPlane.props.scale.y;
+        setPropsNewPlane.position.y -= setPropsNewPlane.scale.y;
+      } else {
+        targetPropsOldPlane.position.y -= oldPlane.props.scale.y;
+        setPropsNewPlane.position.y += setPropsNewPlane.scale.y;
+      }
     } else {
-      // move horizontally
-      targetPropsOldImage.x = direction * oldImage.picture.forPortraitStack.width;
-      newImage.x -= direction * newImage.picture.forPortraitStack.width;
+      if (setPropsNewPlane.anchor.x < .5) {
+        targetPropsOldPlane.position.x -= oldPlane.props.scale.x;
+        setPropsNewPlane.position.x += setPropsNewPlane.scale.x;
+      } else {
+        targetPropsOldPlane.position.x += oldPlane.props.scale.x;
+        setPropsNewPlane.position.x -= setPropsNewPlane.scale.x;
+      }
     }
+    oldPlane.applyProps(setPropsOldPlane);
+    newPlane.applyProps(setPropsNewPlane);
 
-    tl.to(oldImage, {...targetPropsOldImage, duration: slideDuration, ease: Power1.easeInOut}, 0);
-    tl.to(newImage, {...targetPropsNewImage, duration: slideDuration, ease: Power1.easeInOut}, 0);
+    tl.to(oldPlane.props.position, {...targetPropsOldPlane.position, duration: slideDuration, ease: Power1.easeInOut}, 0);
+    tl.to(newPlane.props.position, {...targetPropsNewPlane.position, duration: slideDuration, ease: Power1.easeInOut}, 0);
 
-    this.visibleImages.push(newImage);
+    this.visiblePlanes.push(newPlane);
+    this.addObject(newPlane);
 
     this.scheduleAnimationTimeout();
   };
